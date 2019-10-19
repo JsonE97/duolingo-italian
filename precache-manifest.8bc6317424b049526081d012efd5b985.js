6db7d9d0b424b76bee1a819c2888f85d6c432abd
2191867E <jasonegan59@gmail.com>
2019-10-19 20:57:27 +0100

    Updates

---
+++ precache-manifest.8bc6317424b049526081d012efd5b985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f567147d08937c23ea5e127d9206cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "327f54adf8552d040ec3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9f7a45ab31b0ea781ef4",
+    "url": "/static/js/2.bd5eb61a.chunk.js"
+  },
+  {
+    "revision": "327f54adf8552d040ec3",
+    "url": "/static/js/main.cdf61de8.chunk.js"
+  },
+  {
+    "revision": "3ecce15daa691cd7efee",
+    "url": "/static/js/runtime-main.271cae54.js"
+  }
+]);
---
+++ static/js/main.cdf61de8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-italian"]=this["webpackJsonpreact-italian"]||[]).push([[0],[,,,function(e,n,t){e.exports=t(10)},,,,,function(e,n,t){},function(e,n,t){},function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(2),i=t.n(c);t(8),t(9);var l=function(){return r.a.createElement("div",{className:"App"},r.a.createElement("header",{className:"App-header"},r.a.createElement("p",null,"Duolingo Online")))};i.a.render(r.a.createElement(l,null),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.cdf61de8.chunk.js.map
