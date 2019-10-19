34f85a7fcfb2d38a6041b0cc5b8f8a1a508ca789
2191867E <jasonegan59@gmail.com>
2019-10-19 20:59:50 +0100

    Updates

---
+++ precache-manifest.dcb89d0c2055a3c036391c0a4c61b797.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b551db4e4d962ef87654f4a615810f",
+    "url": "/duolingo-italian/index.html"
+  },
+  {
+    "revision": "327f54adf8552d040ec3",
+    "url": "/duolingo-italian/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9f7a45ab31b0ea781ef4",
+    "url": "/duolingo-italian/static/js/2.bd5eb61a.chunk.js"
+  },
+  {
+    "revision": "327f54adf8552d040ec3",
+    "url": "/duolingo-italian/static/js/main.cdf61de8.chunk.js"
+  },
+  {
+    "revision": "4e94fcd7019b6f2a520f",
+    "url": "/duolingo-italian/static/js/runtime-main.7111abdc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bc6317424b049526081d012efd5b985.js"
+  "/duolingo-italian/precache-manifest.dcb89d0c2055a3c036391c0a4c61b797.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/duolingo-italian/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
