77bc6ecf2d42c99e483835bd32c9750b90765d36
2191867E <jasonegan59@gmail.com>
2020-04-02 19:19:13 +0100

    removing old files

--- src/MyButton.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-export default function MyButton() {
-    return(
-        <div>
-            <input type="checkbox" onChange={() => alert()}></input>
-        </div>
-    )
-}
