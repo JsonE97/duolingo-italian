a53c8b85284e395fb2bde80ca9dc7f9bd4d71f89
2191867E <jasonegan59@gmail.com>
2020-06-04 18:50:44 +0100

    added react spinner for loading

--- src/ParseContainer.jsx
+++ src/ParseContainer.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ParseContainer.scss';
+import Loader from 'react-loader-spinner';
 
 export const ParseContainer = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = () => {
+        setIsLoading(! isLoading);
+    }
+
     return (
         <div className="parser-container-frame">
             <h2>Enter text below:</h2>
             <textarea className="parser-container-entry" type="text"></textarea>
             <div className="parser-options">
-                <button>Submit</button>
+                <button onClick={() => onSubmit()}>Submit</button>
+                <div className="parser-loading-spinner">
+                    {isLoading ?
+                        <Loader
+                            type="Oval"
+                            height="30"
+                            width="30"
+                        >
+
+                        </Loader>
+                    :
+                    <>
+                    </>
+                    }
+                </div>
             </div>
         </div>
     )
