2712e3e45dc452d96221618c1bc6a57a11716cc8
2191867E <jasonegan59@gmail.com>
2020-06-04 18:25:46 +0100

    polished the parser page. now to add the functionality.

--- src/ParseContainer.jsx
+++ src/ParseContainer.jsx
@@ -5,8 +5,10 @@ export const ParseContainer = () => {
     return (
         <div className="parser-container-frame">
             <h2>Enter text below:</h2>
-
-            <input className="parser-container-entry" type="text"></input>
+            <textarea className="parser-container-entry" type="text"></textarea>
+            <div className="parser-options">
+                <button>Submit</button>
+            </div>
         </div>
     )
 }
