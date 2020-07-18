5f914b0c4596eb368a9958cb5aaed9b9b694b649
2191867E <jasonegan59@gmail.com>
2020-07-18 22:23:01 +0100

    added alternative pos-tagging functionality in for doc parser

--- api/parseWords.py
+++ api/parseWords.py
@@ -1,4 +1,5 @@
 from nltk import word_tokenize, pos_tag
+import string
 from googletrans import Translator
 
 translator = Translator()
@@ -21,6 +22,34 @@ def obtain_nouns_verbs(text):
     return {"nouns": nouns, "verbs": verbs}
 
 
+def obtain_nouns_verbs_new(text):
+    it_nouns, it_verbs = {}, {}
+    tokens = text.lower().translate(str.maketrans(
+        '', '', string.punctuation)).split(" ")
+
+    for token in tokens:
+        eng_trans = translator.translate(
+            token, src="it", dest="en").text.lower()
+        token_tag = word_tokenize(eng_trans)
+        pos_tag_obj = pos_tag(token_tag)[0]
+
+        if pos_tag_obj[1] == 'NN' or pos_tag_obj[1] == 'NNS':
+            it_nouns = append_token(it_nouns, token, eng_trans)
+        elif 'VB' in pos_tag_obj[1]:
+            it_verbs = append_token(it_verbs, token, eng_trans)
+
+    return {"it_nouns": it_nouns, "it_verbs": it_verbs}
+
+
+def append_token(dict, token, value):
+    if token not in dict.keys():
+        dict[token] = [value]
+    else:
+        if value not in dict[token]:
+            dict[token].append(value)
+    return dict
+
+
 def apply_translations(tokens, incoming_lang, outgoing_lang):
     translations = {}
     translation = translator.translate(
@@ -32,4 +61,6 @@ def apply_translations(tokens, incoming_lang, outgoing_lang):
 
 
 if __name__ == "__main__":
-    print(obtain_nouns_verbs("when i want to eat a sandwich, i will do it"))
+    #print(obtain_nouns_verbs("when i want to eat a sandwich, i will do it"))
+
+    print(obtain_nouns_verbs_new("sei stato impegnato"))
