562de309e79dd3a360a848f5390f5fdbeab03b2f
2191867E <jasonegan59@gmail.com>
2022-02-08 22:16:11 +0000

    updating basic jsx refactorings

--- src/components/verbs/VerbContainer.jsx
+++ src/components/verbs/VerbContainer.jsx
@@ -2,50 +2,50 @@ import React, { useEffect, useState } from 'react';
 import './VerbContainer.scss';
 
 export const VerbContainer = ({ verbName, verbData }) => {
-    const [indicativo, setIndicativo] = useState([]);
-    useEffect(() => {
-        if (verbData) {
-            setIndicativo(verbData.Indicativo["Indicativo presente"]);
-        }
-    }, [verbData])
-
-    const getEnglishPronounForRow = (i) => {
-        return ["I", "You", "He/She", "We", "You all", "They"][i];
+  const [indicativo, setIndicativo] = useState([]);
+  useEffect(() => {
+    if (verbData) {
+      setIndicativo(verbData.Indicativo["Indicativo presente"]);
     }
+  }, [verbData])
 
-    return (
-        <div className="verbContainer">
-            <table align="center" className="verbTable">
-                <thead>
-                    <tr>
-                        <th colSpan={2}>{verbName} - {verbData.Translation}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        Object.keys(indicativo).map((k, i) => {
-                            return (
-                                <tr>
-                                    <td>{(i === 2 ?
-                                        "lui/lei"
-                                        :
-                                        (i === 5 ?
-                                            "loro"
-                                            :
-                                            k
-                                        )) + ' (' + getEnglishPronounForRow(i) + ')'}</td>
-                                    <td>{indicativo[k]}</td>
-                                </tr>
-                            )
-                        })}
+  const getEnglishPronounForRow = (i) => {
+    return ["I", "You", "He/She", "We", "You all", "They"][i];
+  }
 
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colSpan={2}>test</td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    )
+  return (
+    <div className="verbContainer">
+      <table align="center" className="verbTable">
+        <thead>
+          <tr>
+            <th colSpan={2}>{verbName} - {verbData.Translation}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Object.keys(indicativo).map((k, i) => {
+              return (
+                <tr>
+                  <td>{(i === 2 ?
+                    "lui/lei"
+                    :
+                    (i === 5 ?
+                      "loro"
+                      :
+                      k
+                    )) + ' (' + getEnglishPronounForRow(i) + ')'}</td>
+                  <td>{indicativo[k]}</td>
+                </tr>
+              )
+            })}
+
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={2}>test</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  )
 }
